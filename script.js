03531fdaa26392b16e9b5d4b1c1d0758da7ee34f
Desenvolvedora Salesforce Júnior <111286589+aGaAmorim@users.noreply.github.com>
2026-03-25 11:22:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-var calendar = new FullCalendar.Calendar(calendarEl, {
-  initialView: 'dayGridMonth',
+document.addEventListener('DOMContentLoaded', function () {
+  var calendarEl = document.getElementById('calendar');
 
-  events: [
-    {
-      title: 'Trabalho de História',
-      date: '2026-03-28'
-    },
-    {
-      title: 'Prova de Matemática',
-      date: '2026-03-30'
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+
+    events: [
+      {
+        title: 'Trabalho de História',
+        date: '2026-03-28'
+      },
+      {
+        title: 'Prova de Matemática',
+        date: '2026-03-30'
+      }
+    ],
+
+    dateClick: function(info) {
+      document.querySelector('#formulario input[type="date"]').value = info.dateStr;
+      document.getElementById('formulario').scrollIntoView();
     }
-  ],
+  });
 
-  dateClick: function(info) {
-    document.querySelector('#formulario input[type="date"]').value = info.dateStr;
-  },
-
-  eventClick: function(info) {
-    alert("Atividade: " + info.event.title);
-
-    document.querySelector('#formulario input[type="date"]').value = info.event.startStr;
-
-    document.getElementById('formulario').scrollIntoView();
-  }
+  calendar.render();
 });
